77e6e5133d34f53d179fcb85248d61c1023117ae
Mich <paugam21@outlook.com>
2024-04-19 22:55:29 -0600

    Ruta para registro citas
    Se agregó una ruta para el registro de citas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { RegistroCitasComponent } from './citas/registro-citas/registro-citas.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'regCitas', component:RegistroCitasComponent}
+];
