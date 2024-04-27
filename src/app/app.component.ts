9dd0b24ee7550d0706758bb0203ad30cc7400a07
Mich <paugam21@outlook.com>
2024-04-26 22:27:26 -0600

    Actualizacion
    Actualizar routerModule y agregar animaciones de material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { ServiciosComponent } from "./servicios/servicios.component";
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { ServiciosComponent } from './servicios/servicios.component';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css',
-    imports: [RouterOutlet, ServiciosComponent]
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,RouterModule, ServiciosComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Mini2_AngularTW';
