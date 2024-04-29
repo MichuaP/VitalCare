3bde8a51ef54bd906f969ff846e4640348b5915d
GabySGtz <sanchez.gtz20@gmail.com>
2024-04-28 22:46:16 -0600

    inicio  y array de medicos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { RegistroCitasComponent } from './citas/registro-citas/registro-citas.component';
 import { ServiciosComponent } from './servicios/servicios.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
     { path:'regCitas', component: RegistroCitasComponent },
     { path: 'servicios', component: ServiciosComponent },
+    { path: 'inicio', component: HomeComponent },
+    {path: '**', pathMatch: 'full', redirectTo: 'inicio'}
 ];
