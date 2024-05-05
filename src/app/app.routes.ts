2a6ed5357aaaad3cdc640de8dd82c99b45407f38
Mich <paugam21@outlook.com>
2024-05-05 17:01:01 -0600

    rutas de nosotros

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,9 +6,11 @@ import { MedicosComponent } from './medicos/medicos.component';
 import { ReportesComponent } from './reportes/reportes.component';
 import { UnaespecialidadComponent } from './unaespecialidad/unaespecialidad.component';
 import { SearchComponent } from './search/search.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
 
 export const routes: Routes = [
     { path:'regCitas', component: RegistroCitasComponent },
+    { path:'nosotros', component: NosotrosComponent },
     { path: 'servicios', component: ServiciosComponent },
     { path: 'inicio', component: HomeComponent },
     { path: 'nuestrosMedicos', component: MedicosComponent },
