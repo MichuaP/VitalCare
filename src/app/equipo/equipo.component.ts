2cd763b2bd4cb9d6c3abce0d8ab32b8712c6110e
BarbaraSilvaJ <119126675+BarbaraSilvaJ@users.noreply.github.com>
2024-05-06 11:08:37 -0600

    Avance
    Rutas y equipo

--- src/app/equipo/equipo.component.ts
+++ src/app/equipo/equipo.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from "../footer/footer.component";
+import { MenuComponent } from "../menu/menu.component";
 
 @Component({
-  selector: 'app-equipo',
-  standalone: true,
-  imports: [],
-  templateUrl: './equipo.component.html',
-  styleUrl: './equipo.component.css'
+    selector: 'app-equipo',
+    standalone: true,
+    templateUrl: './equipo.component.html',
+    styleUrl: './equipo.component.css',
+    imports: [FooterComponent, MenuComponent]
 })
 export class EquipoComponent {
 
