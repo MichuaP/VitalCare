c5bb1de34218a13aad18f4e8ada1507cdd8bd4a4
NadiaAcostaM <acostamirandanadiaa@gmail.com>
2024-05-06 20:53:48 -0600

    Changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { MenuComponent } from "../menu/menu.component";
 import { DomseguroPipe } from '../domseguro.pipe';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
-    imports: [MenuComponent, DomseguroPipe]
+    imports: [MenuComponent, DomseguroPipe, RouterModule]
 })
 export class HomeComponent {
     video:string="kkHVpcB4IyU";       
