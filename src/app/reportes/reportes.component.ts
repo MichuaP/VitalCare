26c7c1377f6db75456528d15347402f7842da8de
NadiaAcostaM <acostamirandanadiaa@gmail.com>
2024-05-06 23:17:35 -0600

    Ruta

--- src/app/reportes/reportes.component.ts
+++ src/app/reportes/reportes.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-reportes',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './reportes.component.html',
   styleUrl: './reportes.component.css',
 })
