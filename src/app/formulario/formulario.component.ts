eb440c5c0589ee16aec49825efcd10852be9cef3
WallaceGaster <saitama.kurosakyy@gmail.com>
2024-06-24 12:24:14 -0600

    Cambios en el formulario de la pagina de  Nosotros para que sea reactivo y actualizacion de alerts a sweetalert2

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,19 +1,55 @@
+import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario',
   standalone: true,
-  imports: [FormsModule],
+  imports: [CommonModule ,FormsModule, ReactiveFormsModule],
   templateUrl: './formulario.component.html',
   styleUrl: './formulario.component.css'
 })
 export class FormularioComponent {
   nombre: string="";
   
+  formulario: FormGroup;
+
   @Output() propagar = new EventEmitter<string>();
 
-  onPropagar(){
-    this.propagar.emit(this.nombre);
+  constructor() {
+    this.formulario = new FormGroup({
+      nombre: new FormControl('', [Validators.required, this.nombreValidator]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      tel: new FormControl('', [Validators.required, this.telefonoValidator]),
+      asunto: new FormControl('', [Validators.required]),
+      mensaje: new FormControl('', [Validators.required])
+    });
+  }
+
+  onPropagar() {
+    if (this.formulario.valid) {
+      this.propagar.emit(this.formulario.value);
+    } else {
+      this.formulario.markAllAsTouched();
+    }
+  }
+
+  
+  nombreValidator(control: AbstractControl): { [key: string]: boolean } | null {
+    const nombre = control.value;
+    if (nombre && nombre.split(' ').length < 2) {
+      return { nombreInvalido: true };
+    }
+    return null;
+  }
+
+  
+  telefonoValidator(control: AbstractControl): { [key: string]: boolean } | null {
+    const tel = control.value;
+    const telefonoRegex = /^[0-9]{10}$/; 
+    if (tel && !telefonoRegex.test(tel)) {
+      return { telefonoInvalido: true };
+    }
+    return null;
   }
 }
