a71cd1db1dbe3bde0aaa161ad2fc20183a89ce7d
JalkemMorales <jalkemmorales@gmail.com>
2024-06-25 14:47:20 -0600

    Bugfix general

--- src/app/registro/registro.component.ts
+++ src/app/registro/registro.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 import { Cita, Medico } from '../medico';
 import { MedicoService } from '../shared/medico.service';
 import { Router } from '@angular/router';
 import Swal from 'sweetalert2';
-import { MatDatepickerInputEvent, MatDatepickerModule } from '@angular/material/datepicker';
+import {
+  MatDatepickerInputEvent,
+  MatDatepickerModule,
+} from '@angular/material/datepicker';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatNativeDateModule } from '@angular/material/core';
@@ -18,103 +29,138 @@ import { LoadingService } from '../shared/loading.service';
 @Component({
   selector: 'app-registro',
   standalone: true,
-  imports: [MatInputModule,
+  imports: [
+    MatInputModule,
     MatFormFieldModule,
-    MatDatepickerModule, 
-    MatNativeDateModule, 
+    MatDatepickerModule,
+    MatNativeDateModule,
     MatSelectModule,
     ReactiveFormsModule,
     SweetAlert2Module,
     CommonModule,
-    ResumenComponent],
+    ResumenComponent,
+  ],
   templateUrl: './registro.component.html',
-  styleUrl: './registro.component.css'
+  styleUrl: './registro.component.css',
 })
 export class RegistroComponent {
   //Resumen
-  continuar:boolean=false;
-  
+  continuar: boolean = false;
+
   //Formulario
   citaForm: FormGroup;
-  nombrePac:string="";
-  telefonoPac:string="";
-  apellidosPac:string="";
-  correoPac:string="";
-  contrasenaPac:string="";
-  errorMsg:string="";
+  nombrePac: string = '';
+  telefonoPac: string = '';
+  apellidosPac: string = '';
+  correoPac: string = '';
+  contrasenaPac: string = '';
+  contrasenaRep: string = '';
+  errorMsg: string = '';
 
   //Fechas input material
   fechaAct = new Date();
-  fechaInp:any="";
-  minDate:Date;
-  maxDate:Date;
+  fechaInp: any = '';
+  minDate: Date;
+  maxDate: Date;
 
   //Fechas formateadas
-  fechaSelected:any="";
+  fechaSelected: any = '';
 
   //Hora seleccionada
-  horaSelected:any="";
+  horaSelected: any = '';
 
   //Cosntructor
-  constructor(private fb: FormBuilder, private router:Router, public myAuth: AuthService, private loadingService: LoadingService){
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    public myAuth: AuthService,
+    private loadingService: LoadingService
+  ) {
     //Formulario
-    this.citaForm = this.fb.group({
-      nombre: ['', [Validators.required]],
-      apellidos: ['', [Validators.required]],
-      telefono: ['', [Validators.required]],
-      correo: ['', [Validators.required]],
-      contraseña: ['', [Validators.required]],
-      fecha: ['', [Validators.required]],
-    });
+    this.citaForm = this.fb.group(
+      {
+        nombre: ['', [Validators.required]],
+        apellidos: ['', [Validators.required]],
+        telefono: ['', [Validators.required]],
+        correo: ['', [Validators.required]],
+        contraseña: ['', [Validators.required]],
+        contraseña2: ['', [Validators.required]],
+        fecha: ['', [Validators.required]],
+      },
+      { validators: [this.checkPasswords] }
+    );
     //Establecer fecha actual
     const fechaFormatAct = new Date(this.fechaAct);
     //Establecer la fecha mínima
     this.minDate = this.fechaAct;
   }
 
+  checkPasswords: ValidatorFn = (
+    control: AbstractControl
+  ): ValidationErrors | null => {
+    const password = control.get('contraseña');
+    const password_repeat = control.get('contraseña2');
+
+    return password &&
+      password_repeat &&
+      password.value !== password_repeat.value
+      ? { passwordCoincide: false }
+      : null;
+  };
+
   //Verificar que todo el formulario se llenó
-  checkData(){
+  checkData() {
     this.loadingService.show();
-    const button = document.getElementById('botonRegistrar') as HTMLButtonElement;
+    const button = document.getElementById(
+      'botonRegistrar'
+    ) as HTMLButtonElement;
     button.innerHTML = `
         <div class="spinner-border text-light" role="status">
           <span class="visually-hidden">Loading...</span>
         </div>
       `;
-    if(this.citaForm.valid){   
-      this.loadingService.hide();   
+    if (this.citaForm.valid) {
+      this.loadingService.hide();
       Swal.fire({
-        title: "¿Deseas confirmar tu registro?",
-        showDenyButton: true,
+        title: '¿Deseas confirmar tu registro?',
         showCancelButton: true,
-        confirmButtonText: "Confirmar",
-        denyButtonText: `Cancelar`
+        confirmButtonText: 'Confirmar',
       }).then((result) => {
         if (result.isConfirmed) {
           this.registrarPaciente();
-        } else if (result.isDenied) {
         }
         button.innerHTML = `¡Registrarse!`;
       });
-    }else{
+    } else {
       this.loadingService.hide();
-      Swal.fire('¡Porfavor llene todos los campos!', '', 'error').then((result) => {
-        button.innerHTML = `¡Registrarse!`;
-      });
+      if (
+        JSON.stringify(this.citaForm.errors) == '{"passwordCoincide":false}'
+      ) {
+        Swal.fire('Las constraseñas no coinciden', '', 'error').then(
+          (result) => {
+            button.innerHTML = `Ok`;
+          }
+        );
+      } else {
+        Swal.fire('¡Porfavor llene todos los campos!', '', 'error').then(
+          (result) => {
+            button.innerHTML = `Ok`;
+          }
+        );
+      }
     }
   }
 
   //Función que recibe la fecha seleccionada
   fechaSelec(event: MatDatepickerInputEvent<Date>) {
-    this.fechaInp= event.value;
+    this.fechaInp = event.value;
     //Formatear la fecha
     const fechaFormatInp = new Date(this.fechaInp);
     this.fechaSelected = fechaFormatInp.toLocaleDateString();
   }
 
   //ngOnInit
-  ngOnInit():void{
-  }
+  ngOnInit(): void {}
 
   validate_email(eml: string): boolean {
     const expression = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -126,13 +172,11 @@ export class RegistroComponent {
         title: 'Correo no válido',
         text: 'El correo electrónico no es válido.',
         icon: 'error',
-        confirmButtonText: 'Aceptar'
+        confirmButtonText: 'Aceptar',
       });
       return false;
     }
   }
-  
-  
 
   validate_password(pass: string): boolean {
     if (pass.length < 6) {
@@ -140,14 +184,13 @@ export class RegistroComponent {
         title: 'Contraseña no válida',
         text: 'La contraseña debe tener al menos 6 caracteres.',
         icon: 'error',
-        confirmButtonText: 'Aceptar'
+        confirmButtonText: 'Aceptar',
       });
       return false;
     } else {
       return true;
     }
   }
-  
 
   validate_field(field: any): boolean {
     if (field.length <= 0) {
@@ -155,43 +198,60 @@ export class RegistroComponent {
         title: 'Campo faltante',
         text: 'Falta un campo.',
         icon: 'error',
-        confirmButtonText: 'Aceptar'
+        confirmButtonText: 'Aceptar',
       });
       return false;
     } else {
       return true;
     }
   }
-  
 
   registrarPaciente(): void {
     console.log(this.nombrePac);
     console.log(this.correoPac);
-    if(this.validate_password(this.contrasenaPac) && this.validate_email(this.correoPac) && this.validate_field(this.nombrePac)){
-      this.myAuth.register(this.correoPac, this.nombrePac, this.apellidosPac, this.contrasenaPac, this.telefonoPac, this.fechaSelected).subscribe(() => {
-        Swal.fire({
-          title: 'Éxito!',
-          text: 'Se ha registrado tu usuario correctamente. ¡VitalCare te da la bienvenida!.',
-          icon: 'success',
-          confirmButtonText: '¡Genial!',
-        }).then(() => {
-          this.router.navigate(['/inicio']);
-        });
-      },(error) => {
-        // Error al registrar usuario
-        console.error("Error al registrar usuario:", error);
-        if (error.code === "auth/email-already-in-use") {
-          this.errorMsg = "El correo electrónico ya está en uso. Por favor, intente con otro.";
-        } else {
-          this.errorMsg = "Ocurrió un error durante el registro. Por favor, inténtelo de nuevo más tarde.";
-        }
-        Swal.fire({
-          title: 'Error',
-          text: this.errorMsg,
-          icon: 'error',
-          confirmButtonText: 'Continuar',
-        });
-      });
+    if (
+      this.validate_password(this.contrasenaPac) &&
+      this.validate_email(this.correoPac) &&
+      this.validate_field(this.nombrePac)
+    ) {
+      this.myAuth
+        .register(
+          this.correoPac,
+          this.nombrePac,
+          this.apellidosPac,
+          this.contrasenaPac,
+          this.telefonoPac,
+          this.fechaSelected
+        )
+        .subscribe(
+          () => {
+            Swal.fire({
+              title: 'Éxito!',
+              text: 'Se ha registrado tu usuario correctamente, ya puedes iniciar sesión. ¡VitalCare te da la bienvenida!.',
+              icon: 'success',
+              confirmButtonText: '¡Genial!',
+            }).then(() => {
+              location.reload();
+            });
+          },
+          (error) => {
+            // Error al registrar usuario
+            console.error('Error al registrar usuario:', error);
+            if (error.code === 'auth/email-already-in-use') {
+              this.errorMsg =
+                'El correo electrónico ya está en uso. Por favor, intente con otro.';
+            } else {
+              this.errorMsg =
+                'Ocurrió un error durante el registro. Por favor, inténtelo de nuevo más tarde.';
+            }
+            Swal.fire({
+              title: 'Error',
+              text: this.errorMsg,
+              icon: 'error',
+              confirmButtonText: 'Continuar',
+            });
+          }
+        );
     }
     /*if(newCita){
       Swal.fire({
