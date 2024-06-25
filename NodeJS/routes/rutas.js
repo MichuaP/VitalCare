0f28f77792f5f860eacdcd49b9dfd89e8141181b
Naomi <hernadez.romo.n14@gmail.com>
2024-06-25 15:08:47 -0600

    mover nodejs

--- NodeJS/routes/rutas.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-// Importar el archivo que se encarga de enviar correos
-const correo = require("../correo"); 
-const cita = require("../cita");
-// Definir la ruta para enviar correos
-router.post("/enviar-correo", correo.enviarCorreo); 
-router.post("/enviar-cita", cita.enviarCorreo); 
-module.exports = router;
